bf3b8b26d2562ba0c82d632426f8788032eec757
Pavani-Gannarapu <princesspavani094@gmail.com>
2024-10-17 12:39:38 +0530

    reactjs2

---
+++ test14/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import First from './First'
+import Second from './Second'
+function App() {
+
+
+  return (
+    <>
+   <div>
+     <First/>
+     <Second/>
+   </div>
+    </>
+  )
+}
+
+export default App
---
+++ test14/src/First.jsx
@@ -0,0 +1,13 @@
+function First(){
+    return(
+        <div>
+          <h2>
+              This is First.jsx component
+          </h2>
+          <h3>
+              This is second statement
+          </h3>
+        </div>
+    )
+}
+export default First;
